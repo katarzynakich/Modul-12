9a46f0bbaa3ad7b09e32ee64ac4257e29f7e4f9e
Nemethe <nowakmarceli@gmail.com>
2017-06-27 08:36:17 +0200

    add table generator

--- scripts.js
+++ scripts.js
@@ -16,6 +16,9 @@ function showCountriesList(resp) {
   	countriesList.empty();
 resp.forEach(function(item){
    	$('<li>').text(item.name + ', ' + item.capital + ', ' + item.region + ', ' + item.languages + ', ' + item.currencies ).appendTo(countriesList);
+   	for (var property in item) {
+   		$('#table').append('<tr><td>'+property+'<td><td>'+item[property]+'<td></tr>');
+   	}
 });
 }
 
